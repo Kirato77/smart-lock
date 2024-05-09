6f98e149f960beb57789eddbcdfa80182d9b9b08
Kirato77 <justkirato@gmail.com>
2024-05-09 10:48:15 +0200

    add website

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from machine import Pin
+import network
+import socket
+from time import sleep
+from picozero import pico_temp_sensor, pico_led
 import machine
+import _thread
+
+from machine import Pin
 import utime
 
 # Mappage des touches du clavier à leurs lettres correspondantes
@@ -34,52 +40,87 @@ for i in range(0,4):
     row_pins.append(Pin(keypad_rows[i], Pin.OUT))
     col_pins.append(Pin(keypad_columns[i], Pin.IN, Pin.PULL_DOWN))
 
-def set_angle(angle):
-    duty = int(((angle / 180) * 1000) + 500)
-    pwm.duty_u16(duty * 65535 // 10000)
-    
-def display(decimal):
-    for i in range(3, -1, -1):
-        led_pins[3 - i].value((decimal >> i) & 1)
-        
-def scankeys():
-    for row in range(4):
-        for col in range(4):
-            row_pins[row].high()
-            key = None
-            
-            if col_pins[col].value() == 1:
-                key_press = key_map[row][col]
-                if key_press.isdigit() and 0 <= int(key_press) <= 9:
-                    display(int(key_press))
-                elif key_press == 'E':
-                    display(12)
-                    openDoor()
-                    clear_guess()
-                    continue
-                elif key_press == 'B':
-                    display(15)
-                    clear_guess()
-                elif key_press == 'A':
-                    display(11)
-                    utime.sleep(0.3)
-                    changeCode()
-                    clear_guess()
-                    continue
-                else:
-                    display(15)
-                    
-                print("You pressed", key_press)
-                utime.sleep(0.3)
-                guess.append(key_press)
-                
-        row_pins[row].low()
+ssid = 'JDH-WiFi'
+password = 'IGo1#x15GyBJ0%Gp'
 
+def connect():
+    #Connect to WLAN
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+    while wlan.isconnected() == False:
+        print('Waiting for connection...')
+        sleep(1)
+    ip = wlan.ifconfig()[0]
+    print(f'Connected on {ip}')
+    return ip
+
+def open_socket(ip):
+    # Open a socket
+    address = (ip, 80)
+    connection = socket.socket()
+    connection.bind(address)
+    connection.listen(1)
+    return connection
+
+def webpage(temperature, state):
+    #Template HTML
+    html = f"""
+            <!DOCTYPE html>
+            <html>
+            <form action="./lighton">
+            <input type="submit" value="Light on" />
+            </form>
+            <form action="./lightoff">
+            <input type="submit" value="Light off" />
+            </form>
+            <p>LED is {state}</p>
+            <p>Temperature is {temperature}</p>
+            <form action="./unlock">
+            <input type="submit" value="Unlock" />
+            </form>
+            </body>
+            </html>
+            """
+    return str(html)
+
+def serve(connection):
+    #Start a web server
+    state = 'OFF'
+    pico_led.off()
+    temperature = 0
+    while True:
+        client = connection.accept()[0]
+        request = client.recv(1024)
+        request = str(request)
+        try:
+            request = request.split()[1]
+        except IndexError:
+            pass
+        if request == '/lighton?':
+            pico_led.on()
+            state = 'ON'
+        elif request =='/lightoff?':
+            pico_led.off()
+            state = 'OFF'
+        elif request =='/unlock?':
+            unlock()
+            utime.sleep(2)
+            lock()
+        temperature = pico_temp_sensor.temp
+        html = webpage(temperature, state)
+        client.send(html)
+        client.close()
+        
+def set_angle(angle):
+        duty = int(((angle / 180) * 1000) + 500)
+        pwm.duty_u16(duty * 65535 // 10000)
+        
 def lock():
-    lockedLED.on()
-    unlockLED.off()
-    set_angle(45)  # Déplace le servo à 45 degrés
-    utime.sleep(1)
+        lockedLED.on()
+        unlockLED.off()
+        set_angle(45)  # Déplace le servo à 45 degrés
+        utime.sleep(1)
 
 def unlock():
     lockedLED.off()
@@ -103,65 +144,12 @@ def openDoor():
     else:
         blink_red()
         
-def changeCode():
-    global secret_pin
-    global guess
-    
-    if checkPin():
-        backup_pin = secret_pin
-        secret_pin = []
-
-        print("entered in edit mode")
-        unlockLED.on()
-        clear_guess()
-
-        while True:
-            for row in range(4):
-                for col in range(4):
-                    row_pins[row].high()
-                    key = None
-                    
-                    if col_pins[col].value() == 1:
-                        key_press = key_map[row][col]
-                        if key_press.isdigit() and 0 <= int(key_press) <= 9:
-                            display(int(key_press))
-                        elif key_press == 'B':
-                            print("no modifications")
-                            secret_pin = backup_pin
-                            unlockLED.off()
-                            return None
-                        elif key_press == 'A':
-                            display(11)
-                            utime.sleep(0.5)
-                            if len(secret_pin) > 0:
-                                print("pin modified")
-                            else:
-                                print("no modifications")
-                                secret_pin = backup_pin
-                            unlockLED.off()
-                            return None
-                        else:
-                            display(15)
-                            
-                        print("You pressed", key_press)
-                        utime.sleep(0.3)
-                        if key_press.isdigit() and 0 <= int(key_press) <= 9:
-                            secret_pin.append(key_press)
-                        
-                row_pins[row].low()
-    else:
-        blink_red()
-
-def clear_guess():
-    for x in range(len(guess)):
-        guess.pop()
-        
 def blink_red():
-    for i in range(3):
-        lockedLED.off()
-        utime.sleep(0.5)
-        lockedLED.on()
-        utime.sleep(0.5)
+        for i in range(3):
+            lockedLED.off()
+            utime.sleep(0.5)
+            lockedLED.on()
+            utime.sleep(0.5)
 
 def blink_green():
     for i in range(3):
@@ -170,12 +158,112 @@ def blink_green():
         unlockLED.on()
         utime.sleep(0.5)
         
-print("Enter the password")
-i = 0  
-while True:
-    while i < 1:
+def main():
+    def display(decimal):
+        for i in range(3, -1, -1):
+            led_pins[3 - i].value((decimal >> i) & 1)
+            
+    def scankeys():
+        for row in range(4):
+            for col in range(4):
+                row_pins[row].high()
+                key = None
+                
+                if col_pins[col].value() == 1:
+                    key_press = key_map[row][col]
+                    if key_press.isdigit() and 0 <= int(key_press) <= 9:
+                        display(int(key_press))
+                    elif key_press == 'E':
+                        display(12)
+                        openDoor()
+                        clear_guess()
+                        continue
+                    elif key_press == 'B':
+                        display(15)
+                        clear_guess()
+                    elif key_press == 'A':
+                        display(11)
+                        utime.sleep(0.3)
+                        changeCode()
+                        clear_guess()
+                        continue
+                    else:
+                        display(15)
+                        
+                    print("You pressed", key_press)
+                    utime.sleep(0.3)
+                    guess.append(key_press)
+                    
+            row_pins[row].low()
+            
+    def changeCode():
+        global secret_pin
+        global guess
         
-        lockedLED.on()
-        unlockLED.off()
-        i += 1
-    scankeys()
+        if checkPin():
+            backup_pin = secret_pin
+            secret_pin = []
+
+            print("entered in edit mode")
+            unlockLED.on()
+            clear_guess()
+
+            while True:
+                for row in range(4):
+                    for col in range(4):
+                        row_pins[row].high()
+                        key = None
+                        
+                        if col_pins[col].value() == 1:
+                            key_press = key_map[row][col]
+                            if key_press.isdigit() and 0 <= int(key_press) <= 9:
+                                display(int(key_press))
+                            elif key_press == 'B':
+                                print("no modifications")
+                                secret_pin = backup_pin
+                                unlockLED.off()
+                                return None
+                            elif key_press == 'A':
+                                display(11)
+                                utime.sleep(0.5)
+                                if len(secret_pin) > 0:
+                                    print("pin modified")
+                                else:
+                                    print("no modifications")
+                                    secret_pin = backup_pin
+                                unlockLED.off()
+                                return None
+                            else:
+                                display(15)
+                                
+                            print("You pressed", key_press)
+                            utime.sleep(0.3)
+                            if key_press.isdigit() and 0 <= int(key_press) <= 9:
+                                secret_pin.append(key_press)
+                            
+                    row_pins[row].low()
+        else:
+            blink_red()
+
+    def clear_guess():
+        for x in range(len(guess)):
+            guess.pop()
+            
+    print("Enter the password")
+    i = 0  
+    while True:
+        while i < 1:
+            
+            lockedLED.on()
+            unlockLED.off()
+            i += 1
+        scankeys()
+
+try:
+    _thread.start_new_thread(main, ())
+    
+    ip = connect()
+    connection = open_socket(ip)
+    serve(connection)
+except KeyboardInterrupt:
+    machine.reset()
